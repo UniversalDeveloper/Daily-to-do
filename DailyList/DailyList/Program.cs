bf8b3fca95f67c445055d02c8559c13765ad8c12
Anastasiia Lopata <lopata.ad@gmail.com>
2022-08-24 17:48:22 +0200

    first commit

---
+++ DailyList/DailyList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
