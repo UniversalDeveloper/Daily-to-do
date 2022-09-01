51a7623f6d03c65a5351597c00c93b40269ade50
Anastasiia Lopata <lopata.ad@gmail.com>
2022-09-01 09:53:37 +0200

    #7 created model data for tasks and linled it with window forme

--- DailyList/DailyList/MainWindow.xaml.cs
+++ DailyList/DailyList/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DailyList.DataModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,21 @@ namespace DailyList
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<DataModelOfDailyList> containerForListCases;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void dgToDoList_Loaded(object sender, RoutedEventArgs e)// here will be loaded tascs from file created before
+        {
+            containerForListCases = new BindingList<DataModelOfDailyList>()
+            {
+                new DataModelOfDailyList() { TextOfTasc= "kmfksfk" },
+                new DataModelOfDailyList() { TextOfTasc = "sffsfd" }
+            };
+            dgToDoList.ItemsSource = containerForListCases;// create link of our containerList with vive of Window forme
+
         }
     }
 }
