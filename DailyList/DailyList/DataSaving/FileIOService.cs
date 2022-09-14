ccc23a252a766e1ee5f73f406ae005ed6ebd4cbe
Anastasiia Lopata <lopata.ad@gmail.com>
2022-09-14 12:47:45 +0200

    Created saving method:   1) opened stream for writing use using;   2)created inside blok var wich contained serializable in json format object;   3)recoded serializable object in file;

---
+++ DailyList/DailyList/DataSaving/FileIOService.cs
@@ -0,0 +1,28 @@
+﻿using DailyList.DataModel;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyList.DataSaving
+{ 
+    class FileIOService
+    {  public readonly string PATH;
+        public FileIOService(string path) 
+        { PATH = path;
+        }      
+        public void SaveData(BindingList<DataModelOfDailyList> containerForListCases)
+        {
+            using (StreamWriter saveToFile = File.CreateText(PATH))
+            {
+                var saveInFile = JsonConvert.SerializeObject(containerForListCases);
+                saveToFile.Write(saveInFile);
+            }
+         }
+
+    }
+}
